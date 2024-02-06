fa5706eaa0f84d624b1385ea728f664e642698d5
tbshubham <shubham.goel@tech-bridge.biz>
2024-02-06 11:02:01 +0530

    some code added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 import fs from 'fs';
 import Bencode from 'bencode';
+import dgram from 'dgram';
+import {Buffer} from 'buffer';
+
+const socket = dgram.createSocket('udp4');
+
+const myMsg = Buffer.from("hello ?",'utf8');
 
 const torrent = Bencode.decode(fs.readFileSync('puppy.torrent'));
 

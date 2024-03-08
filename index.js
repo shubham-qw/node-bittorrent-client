c44c2d35a3ff90633096de885658c1b92e5dfc53
shubham-qw <shubhamgoel12345678@gmail.com>
2024-03-08 19:26:52 +0530

    unitarray issue resolved

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict';
 import fs from 'fs';
 import Bencode from 'bencode';
+import dgram from 'dgram';
+import { Buffer } from 'buffer';
+import Url from 'url';
+
+const urlParse = Url.parse
+
+const socket = dgram.createSocket('udp4');
+
+const myMsg = Buffer.from('hello?','utf8');
 
 const torrent = Bencode.decode(fs.readFileSync('puppy.torrent'));
 
-console.log(torrent.announce.toString('utf8'));
+const url = urlParse(new TextDecoder("utf-8").decode(torrent.announce));
+
+console.log(url)
+
+ socket.send(myMsg,0,myMsg.length,url.port,url.host, () => {});
+
+ socket.on('message', msg => {
+    console.log('messgae is', msg);
+ })
+

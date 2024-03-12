1a911228e955965d57a0ea32ab932680efbc4ef0
tbshubham <shubham.goel@tech-bridge.biz>
2024-03-12 16:32:01 +0530

    tracker.js file added

--- tracker.js
+++ tracker.js
@@ -2,4 +2,25 @@
 
 import dgram from 'dgram';
 import {Buffer} from 'buffer';
-import {parse : urlParse} from 'url';
+import {parse} from 'url';
+
+module.exports.getPeers = (torrent, callback) => {
+    const socket = dgram.createSocket('udp4');
+    let url;
+
+    if (typeof(torrent.announce) == 'object') {
+        url = new TextDecoder('utf-8').decode(torrent.announce);
+    } else {
+        url = torrent.announce.toString('utf8');
+    }
+
+    udpSend(socket,buildConnReq(),url);
+}
+
+function udpSend(socket, message, rawUrl, callback = () => {}) {
+    const url = parse(rawUrl);
+}
+
+function buildConnReq() {
+
+}
